644c45f0d1fdf7152ce40b8c5d8eacb63479b69a
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-25 13:01:11 +0500

    router-dom kutubxonasi qoshildi va app js dagi security qoshildi

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,61 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route, useLocation, useNavigate} from "react-router-dom";
 import Login from "./Login.jsx";
 import Register from "./Register.jsx";
 import "rodal/lib/rodal.css"
 import "bootstrap/dist/css/bootstrap.css"
 import Home from "./Home.jsx";
+import {useEffect} from "react";
+import axios from "axios";
 const App = () => {
+
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        handleCheckPermission();
+    }, [pathname]);
+
+    const permissions = [{path: "/home", roles: ["ROLE_MANAGER"]}]
+
+    const handleCheckPermission = () => {
+
+        if (!pathname.startsWith("/login") && !pathname.startsWith("/register")) {
+            if (hasAnyRoles() !== undefined) {
+                axios({
+                    url: "http://localhost:8080/api/auth/me",
+                    method: "GET",
+                    headers: {token: localStorage.getItem("authToken")},
+                }).then((res) => {
+                    console.log(res.data);
+                    for (let i = 0; i < res.data.length; i++) {
+                        if (hasAnyRoles().includes(res.data[i].name)){
+                            return;
+                        }
+                    }
+                    navigate("/login");
+                }).catch(() => {
+                    navigate("/login");
+                    localStorage.removeItem("authToken");
+                })
+            }else {
+                navigate("/login");
+            }
+        }
+    }
+
+    const hasAnyRoles = () => {
+        for (let i = 0; i < permissions.length; i++) {
+            if (pathname.startsWith(permissions[i].path)) {
+                return permissions[i].roles;
+            }
+        }
+    }
     return (
-        <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
-        </Router>
     );
 };
 
--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from "axios";
 import Rodal from "rodal";
 import {useForm} from "react-hook-form";
